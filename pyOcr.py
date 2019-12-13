aec903220bfbd67b25221070a433e251dc4d1cb7
Ryan Yang <Ryanyang99@hotmail.com>
2019-12-13 01:13:33 -0800

    update

--- pyOcr.py
+++ pyOcr.py
@@ -10,6 +10,9 @@ image = image.convert('1', dither=Image.NONE)
 # image = image.convert('1')
 image.save('temp2.jpg')
 
+
+#maybe separate this image to different components and then perform ocr on each of these components
+
 print(tesserocr.image_to_text(image))  # print ocr text from image
 # or
 #print(tesserocr.file_to_text('temp2.jpg'))
