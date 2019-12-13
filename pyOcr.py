6e70267d3bf3da375ceef2fcd3746c9a2f3f86ac
Ryan Yang <Ryanyang99@hotmail.com>
2019-12-13 00:57:47 -0800

    update

---
+++ pyOcr.py
@@ -0,0 +1,15 @@
+import tesserocr
+from PIL import Image, ImageEnhance, ImageFilter
+import sys
+
+image = Image.open(sys.argv[1])
+image = image.convert('1', dither=Image.NONE)
+
+# enhancer = ImageEnhance.Contrast(image)
+# image = enhancer.enhance(2)
+# image = image.convert('1')
+image.save('temp2.jpg')
+
+print(tesserocr.image_to_text(image))  # print ocr text from image
+# or
+#print(tesserocr.file_to_text('temp2.jpg'))
